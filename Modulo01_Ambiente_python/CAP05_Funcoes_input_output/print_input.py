a1f7f8268be220aa959599490aa2afd90b907acc
carlosmandele <carlos.mandelle@gmail.com>
2024-09-30 11:49:51 -0300

    Add funções de entrada e saida

---
+++ Modulo01_Ambiente_python/CAP05_Funcoes_input_output/print_input.py
@@ -0,0 +1,7 @@
+name = input("Informe o seu nome ")
+idade = input("Informe a sua idade ")
+
+print(name, idade)
+print(name, idade, end="...\n")
+print(name, idade, sep="#", end="...\n")
+print(name, idade, sep="#")

7296b18668f3481769ed161675e0a65bea75baa6
carlosmandele <carlos.mandelle@gmail.com>
2024-09-30 11:12:26 -0300

    Add  arquivo variáveis e constantes

---
+++ Modulo01_Ambiente_python/CAP03_Variaveis_e_constantes/variaveis_constantes.py
@@ -0,0 +1,18 @@
+name = "Carlos"
+
+age = 28
+
+print(name, age)
+
+# Alterando o valor de variavel
+name, age  = "Michael", 27
+
+print(name, age)
+
+# Nova variavel com nome sugestivo
+limite_saque_diario = 1000
+
+####### Constantes ##########
+# As variaveis constantes devem ser sempre em maiusculas
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "MG"]
+print(BRAZILIAN_STATES)

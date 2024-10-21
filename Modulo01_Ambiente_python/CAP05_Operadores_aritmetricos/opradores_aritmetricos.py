f03bb069ff2758c8254315ab32fc894ccbcc76fd
carlosmandele <carlos.mandelle@gmail.com>
2024-10-21 08:48:57 -0300

    Add arquivo operadores aritmetricos

---
+++ Modulo01_Ambiente_python/CAP05_Operadores_aritmetricos/opradores_aritmetricos.py
@@ -0,0 +1,26 @@
+produto_1 = 10
+produto_2 = 20
+
+# Adição
+print(produto_1 + produto_2)
+# Subtração
+print(produto_1 - produto_2)
+# Divisãoo
+print(produto_1 / produto_2)
+# Divisão por inteiro
+print(produto_1 // produto_2)
+# Multiplicação
+print(produto_1 * produto_2)
+# Potencia
+print(produto_1 ** produto_2)
+
+# Expressões mais complexas
+a = 10 + 5 * 4
+print(a)
+
+# forçando a precedencia
+a = (10 + 5) * 4
+print(a)
+
+y = (10 / 2) + (25 * 2) - (2**2)
+print(y)
